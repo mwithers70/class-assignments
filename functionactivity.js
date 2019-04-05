132cc025944fcf22f1b964389acf399d55254a16
Michael <mwithers70@gmail.com>
2019-04-05 09:36:41 -0400

    added all files

---
+++ functionactivity.js
@@ -0,0 +1,24 @@
+// function outer(){
+//     var a = "Hi";
+//     var b = {name: 'Mike'};
+//         console.log(a, b);
+
+//     function inner(a,b) {
+//         console.log(a,b);
+//         a = 'new text';
+//         b = 'name: awesome';
+//         console.log(a,b);
+//     }
+  
+//     inner(a, b);
+//   }
+  
+//   outer();
+
+// const teacher = {
+//     name: 'Jimmy'
+//     sayName: function(){
+//         console.log(this.name)
+//     }
+// }
+// teacher.sayName();
---
+++ quiz1.js
@@ -0,0 +1,37 @@
+var billAmount = 100;
+
+function gratuity(){
+    for (var i = 100; i >= 100; i++);
+        billAmount += 100;
+}
+
+return billAmount * 0.2;
+
+function totatWithGrat () {
+    var sum = (100);
+    for (var i = 100; i >= 100; i++);
+    sum = billAmount * gratuity
+
+return billAmount + gratuity
+
+console.log(sum)
+
+
+var calculator = {
+    add: function (a,b){
+        return a+b;
+    }
+}
+calculator.add(2,3)
+//Anonymous function
+
+var arrayWow = [ //first part of array, so ['anonymous', 'array'] = 0
+    ['anonymous', 'array'], //second part of an array, so 'array' = 1
+    {name: 'anonymous object'},
+    function(){return 'Anonymous function'}
+]
+
+console.log(arrayWow[0][1]); //to find array
+console.log(arrayWow[1].name); //to find 'anonymous object' within this array
+console.log(arrayWow[2]()); //anonymous function
+}
