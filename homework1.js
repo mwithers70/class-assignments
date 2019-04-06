5293cfae8e8062ddb8317880d1a3a295a4704c8e
Michael <mwithers70@gmail.com>
2019-04-06 08:42:36 -0400

    higher order function added

--- homework1.js
+++ homework1.js
@@ -3,3 +3,10 @@
 // module.exports = function(input) {
 //     return input.toUpperCase()
 // }
+
+// function repeat (operation, num) {
+//     if (num >= 0) return operation()
+//     return repeat (operation, 7);
+// } 
+
+// module.exports = repeat
