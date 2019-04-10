9492b8ab80b62c1fcc2c42ae019efd7094d20159
Michael <mwithers70@gmail.com>
2019-04-10 08:01:14 -0400

    updating files

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,85 @@
+// console.log('hello');
+
+// var example = 'some string';
+// console.log(example);
+
+// var someString = 'this is a string';
+// console.log(someString);
+
+// var example = 'example string';
+// console.log(example.length);
+
+// var pizza = 'pizza is alright';
+// pizza = pizza.replace('alright','wonderful');
+// console.log(pizza)
+
+// var example = 123456789;
+// console.log(example)
+
+// var roundUp = 1.5;
+// Math.round(0.5);
+
+// var rounded = Math.round(roundUp);
+// console.log(rounded)
+
+// var n = 128;
+// n = n.toString();
+// console.log(n)
+
+// var fruit = 'orange';
+
+// if (fruit.length > 5) {
+//     console.log('The fruit name has more than five characters.');
+// }   else {
+//     console.log('The fruit name has five characters or less');
+// }
+
+// var total = 0;
+// var limit = 10;
+
+// for(var i = 0; i < 10; i ++) {
+//     total += i;}
+//     console.log(total)
+
+
+// var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+//     console.log(pizzaToppings)
+
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,10];
+// var filtered = numbers.filter(function evennumbers (number){
+//     return number % 2 === 0;
+// }
+// )
+// console.log(filtered)
+
+// var food =  ['apple', 'pizza', 'pear'];
+//     console.log(food[1])
+
+// var pets = ['cat', 'dog', 'rat'];
+
+// for(i = 0; i < 3; i++) {
+//     pets[i] = pets[i] + 's'
+// }   console.log(pets)
+
+// var pizza = {
+//     toppings: ['cheese', 'sauce', 'pepperoni'],
+//     crust: 'deep dish',
+//     serves: 2
+// }   
+//     console.log(pizza)
+
+// var food = {
+//     types: 'only pizza'
+// };
+//     console.log(food['types']);
+
+// function eat (food) {
+//     return food + ' tasted really good.';
+//     }
+    
+//     console.log(eat ('bananas'));
+
+function math (firstArg, secondArg, thirdArg) {
+    return (secondArg * thirdArg) + firstArg;
+}
+    console.log(math (53, 61, 67));
