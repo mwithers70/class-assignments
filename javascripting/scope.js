1cf4fe8901b371a925f533e2139e8cf339141ad9
Michael <mwithers70@gmail.com>
2019-04-11 15:49:08 -0400

    updated scope.js

--- javascripting/scope.js
+++ javascripting/scope.js
@@ -79,7 +79,28 @@
     
 //     console.log(eat ('bananas'));
 
-function math (firstArg, secondArg, thirdArg) {
-    return (secondArg * thirdArg) + firstArg;
-}
-    console.log(math (53, 61, 67));
+// function math (firstArg, secondArg, thirdArg) {
+//     return (secondArg * thirdArg) + firstArg;
+// }
+//     console.log(math (53, 61, 67));
+
+var a = 1, b = 2, c = 3;
+
+(function firstFunction(){
+    var b = 5, c = 6;
+
+    (function secondFunction(){
+        var b = 8;
+        console.log("a: "+a+", b: "+b+", c: "+c);
+
+        (function thirdFunction(){
+            var a = 7, c = 9;
+
+            (function fourthFunction(){
+                var a = 1, c = 8;
+
+            })();
+        })();
+    })();
+})();
+
