0b5c7744f5a7f0c09f583d34ba7065d40801acb2
mwithers70 <48702759+mwithers70@users.noreply.github.com>
2019-05-31 08:02:21 -0400

    Add files via upload

---
+++ Prime/src/Prime.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        char again;
+     do {
+         System.out.println("Enter a number: ");
+
+         int x = s.nextInt();
+
+         if (isPrime(x)) {
+             System.out.println(x + " is a prime number");
+         } else {
+             System.out.println(x + " is not a prime number");
+         }
+
+         System.out.println("Want to do another number? ");
+         again = s.next().toLowerCase().charAt(0);
+     }while(again == 'y');
+    }//end of void main
+
+    public static boolean isPrime(int x) {
+        if (x <= 1) {
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(x); i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+            return true;
+    }
+}//end of class Prime
